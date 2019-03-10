f09adc700ad9a6f8f1d862f186bf3ebddec72bd7
Daniel Tonon <daniel.tonon.503@gmail.com>
2019-03-10 15:29:09 +1100

    Adding dynamic recommendation selection

--- atomic-categorizer/src/components/o_recommendation.vue
+++ atomic-categorizer/src/components/o_recommendation.vue
@@ -2,9 +2,9 @@
   article.o-recommendation
     h2.o-recommendation__title Current recommendation
     dl.o-recommendation__list
-      a_score(name="Atom", :value="scores.a", selected=true)
-      a_score(name="Molecule", :value="scores.m", selected=false)
-      a_score(name="Organism", :value="scores.o", selected=false)
+      a_score(name="Atom", :value="scores.a", :selected="selected.a")
+      a_score(name="Molecule", :value="scores.m", :selected="selected.m")
+      a_score(name="Organism", :value="scores.o", :selected="selected.o")
 </template>
 
 <script>
@@ -13,6 +13,25 @@ import a_score from "./a_score.vue";
 export default {
   components: { a_score },
   props: ['scores'],
+  computed: {
+    selected() {
+      const keys = Object.keys(this.scores);
+      const values = Object.values(this.scores);
+      const highestScore = Math.max(...values);
+      const highScores = values.filter((score) => score === highestScore);
+
+      const highScoreIndex = values.indexOf(highestScore);
+      const highScoreKey = keys[highScoreIndex];
+
+      let returnObj = {a:false, m:false, o:false};
+
+      if (highScores.length === 1) {
+        returnObj[highScoreKey] = true;
+      }
+
+      return returnObj;
+    }
+  }
 };
 
 </script>
