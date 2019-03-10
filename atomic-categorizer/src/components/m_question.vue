75306cc63a8dcdb1c387028adca07ac3c4bfc7c2
Daniel Tonon <daniel.tonon.503@gmail.com>
2019-03-10 14:31:27 +1100

    Scores appear under the questions when answered

--- atomic-categorizer/src/components/m_question.vue
+++ atomic-categorizer/src/components/m_question.vue
@@ -5,17 +5,28 @@
         legend.m-question__heading {{question.q}}
         div(v-for="(points, label) in question.a")
           a_radio(:label="label", :name="question.q", :points="points", :index="index" @update="updateScores")
+        dl.m-question__scores(v-if="scores")
+          a_score(name="Atom", :value="`+${scores.a}`")
+          a_score(name="Molecule", :value="`+${scores.m}`")
+          a_score(name="Organism", :value="`+${scores.o}`")
 </template>
 
 <script>
 import a_radio from './a_radio';
+import a_score from './a_score';
 
 export default {
-  components: {a_radio},
+  data(){
+    return {
+      scores: false,
+    }
+  },
+  components: {a_radio, a_score},
   props: ['question', 'index'],
   methods: {
     updateScores (scoreData) {
       this.$emit('update', scoreData)
+      this.scores = scoreData.points;
     }
   },
 };
